02ba399ba0702048224b85fd475947f97d3220c1
Lucas Vieira <lucasltv@gmail.com>
2020-02-11 21:02:13 -0300

    more events

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 8080 })
+const wss = new WebSocket.Server({ port: 3000 })
 
 wss.on('connection', ws => {
     ws.on('message', message => {
         console.log(`Received message => ${message}`)
-    })
-    ws.send('hooo!')
+    });
+    ws.on('disconnect', message => {
+        console.log(`disconnect message => ${message}`)
+    });
+    // ws.send('hooo!');
+    ws.on('open', function open() {
+        console.log('connected');
+        ws.send(Date.now());
+    });
+
+    ws.on('close', function close() {
+        console.log('disconnected');
+    });
 })
